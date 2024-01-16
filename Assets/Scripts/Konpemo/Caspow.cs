bfd790779c162c79dfd71b4302c9ca0cfed2c445
IvanCoudurier <ivan.coudurier@gmail.com>
2024-01-16 11:12:47 +0100

    Update Caspow.cs

--- Assets/Scripts/Konpemo/Caspow.cs
+++ Assets/Scripts/Konpemo/Caspow.cs
@@ -1,17 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
 
 public class Caspow : Konpemo
 {
-    public Caspow() : base(350f, 350f, 5f, 4f, 50f, 1f, 15f, false)
+    public override void SetBaseStats()
     {
-        // Caspow other stats ?
+        health.BaseValue = 350f;
+        health.SetCurrentHealth(350f);
+        strength.BaseValue = 50f;
+        defense.BaseValue = 5f;
+        speed.BaseValue = 4f;
+        attackSpeed.BaseValue = 1f;
+        cooldown.BaseValue = 15f;
+        rangeAttack.BaseValue = 1f;
+        rangeView.BaseValue = 5f;
     }
+
     public override void Attack() // Paralysie 1 chance sur 3
     {
-        this.konpemoEnemy.TakingDamage(this.currentDamage);
+        this.konpemoEnemy.TakingDamage(this.strength.Value);
         System.Random rand = new System.Random();
         int nbParalysed = rand.Next(0, 3);
         if (nbParalysed == 0) { this.konpemoEnemy.isParalysed = true; }
@@ -21,8 +26,8 @@ public class Caspow : Konpemo
     {
         if (this.konpemoEnemy.isPoisoned == false)
         {
-            SetCooldown(this.cooldown);
-            this.konpemoEnemy.Poisoning();
+            SetCooldown(this.cooldown.Value);
+            this.konpemoEnemy.Poisoning(10f, 10);
             
         }
         

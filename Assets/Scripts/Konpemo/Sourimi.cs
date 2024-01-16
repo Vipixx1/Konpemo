29209bd8fc2e548b6232f0354cb3ca88364a7019
IvanCoudurier <ivan.coudurier@gmail.com>
2024-01-16 11:13:12 +0100

    Update Sourimi.cs

--- Assets/Scripts/Konpemo/Sourimi.cs
+++ Assets/Scripts/Konpemo/Sourimi.cs
@@ -1,10 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
-using static Unity.VisualScripting.Member;
-using Unity.VisualScripting;
-using UnityEngine.UIElements;
 
 public class Sourimi : Konpemo
 {
@@ -13,32 +7,39 @@ public class Sourimi : Konpemo
     
     [SerializeField] private Konpemo target; //Pour les tests
 
-    public Sourimi() : base(400f, 400f, 0f, 3f, 150f, 3f, 15f, false)
+    public override void SetBaseStats()
     {
-        //this.range = 3f;
-        //Sourimi other stats ?
+        health.BaseValue = 400f;
+        health.SetCurrentHealth(400f);
+        strength.BaseValue = 150f;
+        defense.BaseValue = 0f;
+        speed.BaseValue = 3f;
+        attackSpeed.BaseValue = 0.3f;
+        cooldown.BaseValue = 15f;
+        rangeAttack.BaseValue = 3f;
+        rangeView.BaseValue = 5f;
     }
 
-    public override void Attack() //Ball'Foudre
+    public override void Attack() // Ball'Foudre
     {
         BallFoudre ballFoudre = Instantiate(ballFoudrePrefab, pointDeTir.position, pointDeTir.rotation);
         Vector3 dirProj = (this.konpemoEnemy.transform.position - pointDeTir.position).normalized;
-        ballFoudre.Setup(dirProj, this.currentDamage);
+        ballFoudre.Setup(dirProj, this.strength.Value);
     }
 
-    public override void Capacity() //Dash quantique
+    public override void Capacity() // Dash quantique
     {
         /*
         Dessine un cercle autour du Sourimi qui montre la range du Dash
         if (Input.GetMouseButtonDown(0))
         {
             Raycast de position de clique souris
-            if (cliqué dans le cercle) 
+            if (clique dans le cercle) 
             {
                 Dash(position clique souris)
             }
             
-            else (ie cliqué hors du cercle)
+            else (ie clique hors du cercle)
             {
                 Nouvelle position, la plus proche du clique et qui est dans le cercle
                 Dash(Nouvelle position)

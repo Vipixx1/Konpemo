18d8a2781a8df2b5b2cd90ee772eba00f27ff78b
IvanCoudurier <ivan.coudurier@gmail.com>
2024-01-16 11:13:16 +0100

    Update Health.cs

--- Assets/Scripts/Konpemo/Stats/Health.cs
+++ Assets/Scripts/Konpemo/Stats/Health.cs
@@ -1,8 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : CharacterStat
 {
+    protected float currentHealth;
 
+    public void SetCurrentHealth(float currentHealth)
+    {
+        this.currentHealth = currentHealth;
+    }
+
+    public void TakingFlatDamage(float rawDamage, float defense)
+    {
+        this.currentHealth -= Math.Max(1, rawDamage - defense);
+    }
+
+    public void HealingFlatDamage(float healValue)
+    {
+        this.currentHealth = Math.Min(this.BaseValue, this.currentHealth + healValue);
+    }
 }

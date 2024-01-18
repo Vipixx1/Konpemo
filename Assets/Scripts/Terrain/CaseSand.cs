91a8eb7366290204fb0f85bb7ae3c768c683e393
IvanCoudurier <ivan.coudurier@gmail.com>
2024-01-18 10:19:18 +0100

    Update CaseSand.cs

--- Assets/Scripts/Terrain/CaseSand.cs
+++ Assets/Scripts/Terrain/CaseSand.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class CaseSand : MonoBehaviour
 {
-    private float percentSpeedReduction = -0.25f;
+    private float percentSpeedReduction = -0.1f;
     private void OnTriggerEnter(Collider other)
     {
-        Konpemo konpemo = other.GetComponent<Konpemo>();  
-        if (konpemo != null)
+        if (other.TryGetComponent<Konpemo>(out var konpemo))
         {
-            konpemo.speed.AddModifier(new StatModifier(percentSpeedReduction, StatModType.PercentAdd, this));
+            //Debug.Log("a : " + konpemo.speed.Value);
+            if (konpemo.speed.StatModifiers.Count > 0)
+            {
+                bool isOnSand = false;
+                foreach (StatModifier speedMod in konpemo.speed.StatModifiers)
+                {
+                    if (speedMod.Source.ToString() == "CaseSand") { isOnSand = true; }
+                }
+
+                if (! isOnSand)
+                {
+                    konpemo.speed.AddModifier(new StatModifier(percentSpeedReduction, StatModType.PercentAdd, "CaseSand"));
+                    //Debug.Log("b : " + konpemo.speed.Value);
+                }
+
+            }
+            else
+            {
+                konpemo.speed.AddModifier(new StatModifier(percentSpeedReduction, StatModType.PercentAdd, "CaseSand"));
+                //Debug.Log("c : " + konpemo.speed.Value);
+            }
+            
         }
     }
 
@@ -19,7 +39,7 @@ public class CaseSand : MonoBehaviour
         Konpemo konpemo = other.GetComponent<Konpemo>();
         if (konpemo != null)
         {
-            konpemo.speed.RemoveAllModifiersFromSource(this);
+            konpemo.speed.RemoveAllModifiersFromSource("CaseSand");
         }
     }
 }
